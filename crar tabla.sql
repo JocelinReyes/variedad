f8abc61b1fef2024de9e224e8447202574be391a
JocelinReyes <160088658+JocelinReyes@users.noreply.github.com>
2024-09-13 20:46:27 -0600

    Create crar tabla.sql

---
+++ crar tabla.sql
@@ -0,0 +1,9 @@
+CREATE TABLE Animales(
+noAnimal INTEGER (3) NOT NULL,
+tipo VARCHAR (20) NOT NULL,
+genero VARCHAR (20) NOT NULL,
+edad INTEGER (3) NOT NULL,
+CONSTRAINT Animales_noAnimal_pk
+PRIMARY KEY (noAnimal)
+);
+--Este es un ejemplo de como crear una tabla en nuestra base de datos--
