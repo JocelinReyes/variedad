355221f279e7bb07b008f1d8c7d57e6c974931ce
JocelinReyes <160088658+JocelinReyes@users.noreply.github.com>
2024-09-16 16:17:54 -0600

    Update crar tabla.sql

--- crar tabla.sql
+++ crar tabla.sql
@@ -7,3 +7,18 @@ CONSTRAINT Animales_noAnimal_pk
 PRIMARY KEY (noAnimal)
 );
 --Este es un ejemplo de como crear una tabla en nuestra base de datos--
+CREATE TABLE Vacunas(
+idVacuna INTEGER (3) NOT NULL,
+descripcion VARCHAR (20) NOT NULL,
+CONSTRAINT Vacunas_idVacuna_pk
+PRIMARY KEY (idVacuna)
+);
+
+CREATE TABLE VacunacionAnimal(
+idVacuna INT,
+noAnimla INT,
+fechaAplicacion DATE,
+PRIMARY KEY (noAnimal, idVacuna),
+FOREIGN KEY (idVacuna) REFERENCES Vacunas (idVacuna),
+FOREIGN KEY (noAnimal) REFERENCES Animales (noAnimal)
+);
